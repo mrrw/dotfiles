c67a54361bdab220efeeca8f7a21b55990656d59
calciumcyborg <52546966+calciumcyborg@users.noreply.github.com>
2019-09-09 23:57:47 -0400

    Add files via upload

---
+++ start_jack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# starts JACK instance.  Execute in xinitrc.
+
+jack_control start
+jack_control ds alsa
+jack_control dps device hw:HD2
+jack_control dps rate 48000
+jack_control dps nperiods 2
+jack_control dps period 64
+sleep 10
+a2jmidid -e &
+sleep 10
+qjackctl &
+
